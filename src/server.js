6ba8370d8a24f4b20d63278b7b0b75ce3092cddf
Фёдор Стрельников <strel.fed2002@mail.ru>
2022-05-18 00:28:17 +0300

    heroking

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/SafeChatFrontEnd'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname + '/SafeChatFrontEnd/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/SafeChatFrontEnd/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
