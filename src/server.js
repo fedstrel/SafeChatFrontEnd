37acf5a8fff8f5a407b6eab11ff48f9500bafadf
Фёдор Стрельников <strel.fed2002@mail.ru>
2022-05-18 00:39:59 +0300

    heroking

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 //Install express server
+const _dirname = dirname(fileURLToPath(import.meta.url));
+
 const express = require('express');
 const path = require('path');
+const {dirname} = require("@angular/compiler-cli");
+const {fileURLToPath} = require("url");
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/SafeChatFrontEnd'));
+app.use(express.static(_dirname + '/SafeChatFrontEnd'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname + '/SafeChatFrontEnd/src/index.html'));
+  res.sendFile(path.join(_dirname + '/SafeChatFrontEnd/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
